b061a5550180490a38f08381e118953fe210d6cb
PJ Kelly <me@pjkel.ly>
2013-10-19 16:29:28 -0700

    Refactored tests.

--- test/shellVarParser.js
+++ test/shellVarParser.js
@@ -1,38 +1,35 @@
 var assert         = require('assert'),
     should         = require('should'),
-    fs             = require('fs'),
     shellVarParser = require('../lib/shellVarParser');
+
+var parsed = function(str) {
+  return shellVarParser.parse(str);
+}
 
 describe('shellVarParser', function() {
   describe('.parse()', function() {
-    var env;
-
-    before(function() {
-      env = shellVarParser.parse(fs.readFileSync(__dirname + '/.env', 'utf8'));
-    });
-
     it('sets the basic environment variables', function() {
-      env.BASIC.should.eql("basic");
+      parsed('BASIC=basic').BASIC.should.eql("basic");
     });
 
     it('sets double quotes environment variables', function() {
-      env.DOUBLE_QUOTES.should.eql("double_quotes");
+      parsed('DOUBLE_QUOTES="double_quotes"').DOUBLE_QUOTES.should.eql("double_quotes");
     });
 
     it('sets single quotes environment variables', function() {
-      env.SINGLE_QUOTES.should.eql("single_quotes");
+      parsed("SINGLE_QUOTES='single_quotes'").SINGLE_QUOTES.should.eql("single_quotes");
     });
 
     it('expands newlines if double quoted', function() {
-      env.EXPAND_NEWLINES.should.eql("expand\nnewlines");
+      parsed('EXPAND_NEWLINES="expand\\nnewlines"').EXPAND_NEWLINES.should.eql("expand\nnewlines");
     });
 
     it('does not expand newlines if unquoted', function() {
-      env.DONT_EXPAND_NEWLINES_UNQUOTED.should.eql("dontexpand\\nnewlines");
+      parsed('DONT_EXPAND_NEWLINES_UNQUOTED=dontexpand\\nnewlines').DONT_EXPAND_NEWLINES_UNQUOTED.should.eql("dontexpand\\nnewlines");
     });
 
     it('does not expand newlines if single-quoted', function() {
-      env.DONT_EXPAND_NEWLINES_SINGLE_QUOTED.should.eql("dontexpand\\nnewlines");
+      parsed("DONT_EXPAND_NEWLINES_SINGLE_QUOTED='dontexpand\\nnewlines'").DONT_EXPAND_NEWLINES_SINGLE_QUOTED.should.eql("dontexpand\\nnewlines");
     });
   });
 });
